f6e4215c8b190677d10c012bafa50d365f80e6e5
Jacob Fisher <jacobfisher@Mac.fios-router.home>
2025-10-03 00:05:29 -0400

    unsafe

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Helmet } from "react-helmet";
 import logo from "./assets/Y-Logo.png"; // ✅ exact file name
 
